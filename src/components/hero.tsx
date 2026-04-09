b3d04bba944e43b6125a116fcbcaf50de40ee0dc
Muhammad Shaheer <skhan1256787@gmail.com>
2026-04-10 03:59:06 +0500

    fix(ui): smooth gradient merge in hero section and styled about us

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -76,7 +76,7 @@ export default function Hero() {
       {/* Bottom gradient background */}
       <div
         aria-hidden="true"
-        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu blur-3xl sm:top-[calc(100%-30rem)] pointer-events-none"
+        className="absolute inset-x-0 top-[calc(100%-25rem)] -z-10 transform-gpu blur-3xl sm:top-[calc(100%-45rem)] pointer-events-none"
       >
         <div
           style={{
